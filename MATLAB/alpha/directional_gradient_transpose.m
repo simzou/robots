c56699195cba87350fc39c8818e92e9cf79c7e25
mitchellhorning <mhorning@hmc.edu>
2014-07-07 14:24:15 -0700

    deleted:    directional_gradient.asv 	modified:   directional_gradient.m 	modified:   directional_gradient_transpose.m

--- MATLAB/alpha/directional_gradient_transpose.m
+++ MATLAB/alpha/directional_gradient_transpose.m
@@ -1,10 +1,13 @@
 function [dx,dy] = directional_gradient_transpose(u, m, n)
-% computes a directional gradient
-% takes in a 1-D vector (or 2-D) and dimensions of matrix A
-% returns two matrices as vectors
-% one is horizontal gradient and the other vertical gradient
-% dx_ij = A_(i-1)j - A_ij, for the first column i-1 refers to the last column
-% dy_ij = A_i(j-1) - A_ij, for the first row j-1 refers to the last row
+% This function returns the gradient transposes of the mxn image u in the
+% horizontal direction and the vertical direction. u is expected to be 
+% input as a one dimensional vector. dx and dy are also one dimensional 
+% vectors. The gradients are computed as
+%
+%     del_x u_{i,j} = u_{i,(j+1) % n} - u_{i,j}
+%     del_x u_{i,j} = u_{(i+1) % m, j} - u_{i,j}
+%
+
 	A = reshape(u, m, n);
 	dx = [A(:,end) - A(:,1)  A(:,1:end-1) - A(:,2:end)];
 	dy = [A(end,:) - A(1,:); A(1:end-1,:) - A(2:end,:)];
