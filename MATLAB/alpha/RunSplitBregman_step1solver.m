5b7e9493e140693efc6d29e414bc84a969a146ae
kyin7 <kyin@math.ucla.edu>
2014-07-09 08:56:40 -0700

    hand-tuned parameters by Ke

--- MATLAB/alpha/RunSplitBregman_step1solver.m
+++ MATLAB/alpha/RunSplitBregman_step1solver.m
@@ -1,7 +1,7 @@
 clc; clear all;
 
-dim = 10;
-num_paths = 10;
+dim = 30;
+num_paths = 200;
 mu = 1;
 lambda1 = .1;
 lambda2 = .1;
@@ -28,7 +28,7 @@ Phi3 = @(u) directional_gradient_y(u, m, n);
 
 uguess = genSplitBregman_step1solver( Phi1, Phi2, Phi3, A, g, m, n, mu, lambda1, lambda2, tol, N);
 
-error  = norm(u-uguess)
+error  = norm(u-uguess);
 u      = reshape(u,dim,dim);
 uguess = reshape(uguess,dim,dim);
 
@@ -46,7 +46,7 @@ subplot(1,3,3);
 imagesc(weights);
 title('Paths')
 
-[u uguess]
+% [u uguess]
 toc;
 hold off
 profile viewer;
