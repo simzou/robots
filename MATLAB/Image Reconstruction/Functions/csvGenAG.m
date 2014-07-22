2eb7a2d6533e8cf83c933dcce8a47c70ea8a10e6
mitchellhorning <mhorning@hmc.edu>
2014-07-22 13:13:18 -0700

    new file:   MATLAB/Image Reconstruction/Figures/smallareas/smallAdapt100.fig 	new file:   MATLAB/Image Reconstruction/Figures/smallareas/smallAdapt500.fig 	new file:   MATLAB/Image Reconstruction/Figures/smallareas/smallBounce100.fig

---
+++ MATLAB/Image Reconstruction/Functions/csvGenAG.m
@@ -0,0 +1,18 @@
+function [ A g ] = csvGenAG( csv_path, dim )
+%CSVSPLITBREGMANSOLVE generates A and g given a csv with paths & integrals.
+%   csv should have 1 line per data point of this format:
+%      xstart, ystart, xend, yend, int
+%
+
+csv = csvread(csv_path);
+
+paths = csv(:,1:end-1);
+g = csv(:, end);
+u = zeros(dim);
+
+[A, ~, ~] = generateAug( u, paths);
+
+
+
+end
+
