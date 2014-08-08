e922226255777cb1aeeedc4db06af5e211a36b31
mitchellhorning <mhorning@hmc.edu>
2014-08-08 15:24:01 -0700

    new file:   MATLAB/Image Reconstruction/Functions/genNextTargets.m

---
+++ MATLAB/Image Reconstruction/Functions/genNextTargets.m
@@ -0,0 +1,20 @@
+function newpaths = genNextTargets( data, dim, scale, numpaths )
+
+data = data.*scale;
+paths = data( :, 1:end-1 );
+u = zeros(dim);
+g = data( :, end );
+[A,u,~] = generateAug( u, paths );
+
+param.tolSB = 1/255;
+
+uguess= splitBregmanSolve( A, g, u, dim, param );
+points = segmentImgGradient(uint8(uguess),dim);
+
+if isempty(points)
+    newpaths = generatePaths(numpaths, dim, 'randombounce');
+else
+    newpaths = generatePaths(numpaths, dim, 'centered', points);
+end
+
+end
